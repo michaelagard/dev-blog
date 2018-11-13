988e1bc0670acab05d17768df8f6500454eed97b
michaelagard <mdagard@gmail.com>
2018-11-12 19:28:05 -0500

    add fix for typography deploy

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,8 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
-// import GitHub from 'typography-theme-github' // Not needed now
+// import Wordpress2016 from 'typography-theme-wordpress-2016'
+import GitHub from 'typography-theme-github' // Not needed now
 
-Wordpress2016.overrideThemeStyles = () => ({
+GitHub.overrideThemeStyles = () => ({
   'a.gatsby-resp-image-link': {
     boxShadow: 'none',
   },
@@ -17,9 +17,9 @@ Wordpress2016.overrideThemeStyles = () => ({
   }
 })
 
-delete Wordpress2016.googleFonts
+delete GitHub.googleFonts
 
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(GitHub)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
