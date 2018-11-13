116588df0d06fc6e1e951693bc14969a8a9eaae2
michaelagard <mdagard@gmail.com>
2018-11-12 19:44:02 -0500

    added blockquote styles

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -14,6 +14,10 @@ GitHub.overrideThemeStyles = () => ({
   },
   'h1,h2,h3,h4,h5,h6': {
     color: '#d4d4dcda'
+  },
+  'blockquote': {
+    color: "grey",
+    borderLeft: "10px solid #ccc",
   }
 })
 
